a94011e579b81c917efd2b1105cae065833b4774
THitokuse <hasegawa.taichiro@gmail.com>
2019-05-02 03:31:44 +0900

    レンダラーを作成

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
   const width = 960;
   const height = 540;
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#myCanvas')
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
 }
